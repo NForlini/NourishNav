0f308ac0b68a3ba0a12566cfdcf998afffc22ef4
NForlini <ninaforlini1@gmail.com>
2024-03-30 00:49:19 +0000

    edits to syntax of recipeCard

--- src/front/js/component/recipes/recipeCard.js
+++ src/front/js/component/recipes/recipeCard.js
@@ -5,28 +5,39 @@ import { Context } from "../store/appContext";
 export default function recipeCard() {
   const [recipe, setRecipe] = useState([]);
   const { store, actions } = useContext(Context);
-  useEffect(() => {
-    //how to get to do a blank query instead of mushroom risotto?   ${query}
-    var query = "mushroom risotto";
 
-    fetch("https://api.calorieninjas.com/v1/recipe?query=" + query, {
-      method: "GET",
-      headers: {
-        "X-Api-Key": "a6SJ0mhQQeAcTQWuf6iKvQ==eL9qCOhw9zSvjNxM",
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((error) => {
-        console.error("Error: ", error);
+  useEffect(() => {
+    // Does this search work?
+    document
+      .getElementById("searchForm")
+      .addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        var query = document.getElementById("searchInput").value;
+
+        fetch(
+          "https://api.calorieninjas.com/v1/nutrition?query=" +
+            encodeURIComponent(query),
+          {
+            method: "GET",
+            headers: {
+              "X-Api-Key": "a6SJ0mhQQeAcTQWuf6iKvQ==eL9qCOhw9zSvjNxM",
+              "Content-Type": "application/json",
+            },
+          }
+        )
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json();
+          })
+          .then((result) => {
+            console.log(result);
+          })
+          .catch((error) => {
+            console.error("Error: ", error);
+          });
       });
   }, []);
 
@@ -40,18 +51,19 @@ export default function recipeCard() {
 
   return (
     <div className="d-flex col-10 mx-auto overflow-auto">
-      {recipe?.map((recipe, index) => (
+      {recipe?.map((recipeItem, index) => (
         <div key={index} className="card" style={{ minWidth: "18rem" }}>
           <img src="#" className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{recipe.name}</h5>
+            <h5 className="card-title">{recipeItem.name}</h5>
+            {/* I don't think this link is correct because I don't have a recipeDescription.js */}
             <Link
-              to={"recipe_description/" + recipe.uid}
+              to={"recipeDescription/" + recipeItem.uid}
               className="btn btn-primary"
             >
               Learn More
             </Link>
-            <button onClick={() => handleFavorites(recipe.name)}>💖</button>
+            <button onClick={() => handleFavorites(recipeItem.name)}>💖</button>
           </div>
         </div>
       ))}
